657eb89a8779c148e1f5251eb36d2d3de12538ff
lch09253 <lch09253@gmail.com>
2023-02-22 13:21:44 +0900

    Set CartContext

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export function CartContextProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ cart, setCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCartContext() {
+  return useContext(CartContext);
+}
